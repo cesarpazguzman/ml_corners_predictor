8513f8c83baee9545b39bd36bbc03f64067f5084
Cesar Paz <cesar.pazguzman@gmail.com>
2021-08-23 21:53:14 +0200

    Liquibase database creation

---
+++ DataEnginnering/DatabaseCreation/src/main/sql/ddl/active_matches.sql
@@ -0,0 +1,5 @@
+CREATE TABLE football_data.active_matches
+(
+    ID INTEGER NOT NULL,
+    URL VARCHAR(255) NOT NULL
+)
---
+++ DataEnginnering/DatabaseCreation/src/main/sql/ddl/finished_matches.sql
@@ -0,0 +1,5 @@
+CREATE TABLE football_data.finished_matches
+(
+    ID INTEGER NOT NULL,
+    URL VARCHAR(255) NOT NULL
+)
