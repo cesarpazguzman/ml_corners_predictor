83bdb6c1ac5dfaad115e9099c73cf4be54bf6349
Cesar Paz <cesar.pazguzman@gmail.com>
2021-08-24 21:40:11 +0200

    Add records in season_leagues_url

---
+++ DataEnginnering/DatabaseCreation/src/main/sql/dml/season_league_url.sql
@@ -0,0 +1,35 @@
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (1,"https://www.flashscore.es/futbol/espana/laliga-2020-2021/resultados/","2020/2021","SPAIN");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (2,"https://www.flashscore.es/futbol/espana/laliga-2019-2020/resultados/","2019/2020","SPAIN");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (3,"https://www.flashscore.es/futbol/espana/laliga-2018-2019/resultados/","2018/2019","SPAIN");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (4,"https://www.flashscore.es/futbol/espana/laliga-2017-2018/resultados/","2017/2018","SPAIN");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (5,"https://www.flashscore.es/futbol/espana/laliga-2016-2017/resultados/","2016/2017","SPAIN");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (6,"https://www.flashscore.es/futbol/espana/laliga-2015-2016/resultados/","2015/2016","SPAIN");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (7,"https://www.flashscore.es/futbol/espana/laliga-2014-2015/resultados/","2014/2015","SPAIN");
+
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (8,"https://www.flashscore.es/futbol/inglaterra/premier-league-2020-2021/resultados/","2020/2021","PREMIER");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (9,"https://www.flashscore.es/futbol/inglaterra/premier-league-2019-2020/resultados/","2019/2020","PREMIER");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (10,"https://www.flashscore.es/futbol/inglaterra/premier-league-2018-2019/resultados/","2018/2019","PREMIER");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (11,"https://www.flashscore.es/futbol/inglaterra/premier-league-2017-2018/resultados/","2017/2018","PREMIER");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (12,"https://www.flashscore.es/futbol/inglaterra/premier-league-2016-2017/resultados/","2016/2017","PREMIER");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (13,"https://www.flashscore.es/futbol/inglaterra/premier-league-2015-2016/resultados/","2015/2016","PREMIER");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (14,"https://www.flashscore.es/futbol/inglaterra/premier-league-2014-2015/resultados/","2014/2015","PREMIER");
+
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (15,"https://www.flashscore.es/futbol/italia/serie-a-2020-2021/resultados/","2020/2021","CALCIO");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (16,"https://www.flashscore.es/futbol/italia/serie-a-2019-2020/resultados/","2019/2020","CALCIO");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (17,"https://www.flashscore.es/futbol/italia/serie-a-2018-2019/resultados/","2018/2019","CALCIO");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (18,"https://www.flashscore.es/futbol/italia/serie-a-2017-2018/resultados/","2017/2018","CALCIO");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (19,"https://www.flashscore.es/futbol/italia/serie-a-2016-2017/resultados/","2016/2017","CALCIO");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (20,"https://www.flashscore.es/futbol/italia/serie-a-2015-2016/resultados/","2015/2016","CALCIO");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (21,"https://www.flashscore.es/futbol/italia/serie-a-2014-2015/resultados/","2014/2015","CALCIO");
+
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (22,"https://www.flashscore.es/futbol/alemania/bundesliga-2020-2021/resultados/","2020/2021","BUNDESLIGA");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (23,"https://www.flashscore.es/futbol/alemania/bundesliga-2019-2020/resultados/","2019/2020","BUNDESLIGA");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (24,"https://www.flashscore.es/futbol/alemania/bundesliga-2018-2019/resultados/","2018/2019","BUNDESLIGA");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (25,"https://www.flashscore.es/futbol/alemania/bundesliga-2017-2018/resultados/","2017/2018","BUNDESLIGA");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (26,"https://www.flashscore.es/futbol/alemania/bundesliga-2016-2017/resultados/","2016/2017","BUNDESLIGA");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (27,"https://www.flashscore.es/futbol/alemania/bundesliga-2015-2016/resultados/","2015/2016","BUNDESLIGA");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (28,"https://www.flashscore.es/futbol/alemania/bundesliga-2014-2015/resultados/","2014/2015","BUNDESLIGA");
+
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (29,"https://www.flashscore.es/futbol/francia/ligue-1-2020-2021/","2020/2021","LIGUE1");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (30,"https://www.flashscore.es/futbol/francia/ligue-1-2019-2020/","2019/2020","LIGUE1");
+INSERT INTO football_data.season_leagues_url (ID, URL, SEASON, LEAGUE) VALUES (31,"https://www.flashscore.es/futbol/francia/ligue-1-2018-2019/","2018/2019","LIGUE1");
